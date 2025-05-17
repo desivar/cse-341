72e31897971557f3757dd528367b49ce05b5c159
desivar <jillian_desire@yahoo.es>
2025-05-16 22:08:33 -0600

    update controllers

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,33 +1,100 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
-const { MongoClient } = require('mongodb'); // Corrected import
-
-let database; // Changed _db to database for consistency with the rest of the code
-
-const initDb = callback => {
-    if (database) {
-        console.log('Database is already initialized!');
-        return callback(null, database);
-    }
-    MongoClient.connect(process.env.MONGODB_URL)
-        .then(client => {
-            database = client.db();
-            callback(null, database);
-        })
-        .catch(err => {
-            callback(err);
-        });
+const getAll = (req, res) => {
+  mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
 };
 
-const getDatabase = () => {
-    if (!database) {
-        throw new Error('Database not initialized');
-    }
-    return database;
+const getSingle = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a contact.');
+  }
+  const userId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .find({ _id: userId })
+    .toArray((err, result) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result[0]);
+    });
+};
+
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to update a contact.');
+  }
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to delete a contact.');
+  }
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
 };
 
 module.exports = {
-    initDb,
-    getDatabase
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
